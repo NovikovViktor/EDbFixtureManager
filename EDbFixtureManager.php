5d41c24c2593ba7397c08eac60fcc6e7e0cacb8c
Novikov Viktor <viktor.novikov95@gmail.com>
2017-11-16 22:09:34 +0200

    Removed overhead functionality, updated README

--- EDbFixtureManager.php
+++ EDbFixtureManager.php
@@ -5,9 +5,8 @@
  * Available command properties:
  *    pathToFixtures - path to your fixtures file
  *    modelsFolder   - path to folder where your models classes lay, default to `application.models.*`
- *    php_sql_parser - path to PHPSQLParser class file (optional)
  *
- * Additions:
+ * Note:
  * 1) All attributes you want to fill via fixtures data, must be defined with `safe` validation rule (`rules()` method);
  * 2) Don't forget configure `tablePrefix` option for `db` connection definition;
  * 3) Your tables will be purged when you loading fixtures;
@@ -18,92 +17,40 @@ class EDbFixtureManager extends CConsoleCommand
 {
     public $pathToFixtures;
     public $modelsFolder;
-    public $php_sql_parser;
 
     /**
      * Load fixtures into database from fixtures file
      */
-    public function actionLoad($truncateMode = false)
+    public function actionLoad()
     {
-        $startTime = microtime(true); // check time of start execution script
-        $startMemory = round(memory_get_usage(true) / 1048576, 2);
-
         echo "\033[36m Are you sure you want to load fixtures? Your database will be purged! [Y/N] \033[0m";
 
-        $handle = fopen("php://stdin", "r");
-        $line = fgets($handle);
-        $purgedLine = preg_replace('/[^A-Za-z0-9\-]/', '', $line); // or trim($line)
+        $phpStdin     = fopen("php://stdin", "r");
+        $inputValue   = fgets($phpStdin);
+        $purifiedLine = preg_replace('/[^A-Za-z0-9\-]/', '', $inputValue);
 
-        if (strtolower($purgedLine) == 'n') {
+        if (strtolower($purifiedLine) == 'n') {
             echo "\033[34m Stopping the executing... Done. \033[0m \n";
             die;
         }
 
-        // assign file variable what consist full path to fixtures file
-        $file = empty($this->pathToFixtures) ? __DIR__ . '/fixtures.php' : $this->pathToFixtures;
-
-        // import models classes to make available create new instances
-        if (empty($this->modelsFolder)) { // if property empty default load form the models folder in protected directory
-            Yii::import('application.models.*');
-        } else {
-            if (is_array($this->modelsFolder)) { // if modelsFolder defined check if it array of models
-                foreach ($this->modelsFolder as $key => $folder) {
-                    Yii::import($folder); //import each folder form array
-                }
-            } else {
-                Yii::import($this->modelsFolder); //import single models folder
-            }
-        }
-
-        if (!file_exists($file)) { // check if exist file with fixtures
+        if (empty($this->pathToFixtures) || !file_exists($this->pathToFixtures)) {
             echo "\033[33m There is no file with fixtures to load! Make sure that you create file with fixtures,
                   or pass correct file name \033[0m \n";
             die;
         }
 
-        $fixtures = require_once $file; // require that file with fixtures, will be array
-        $errorList = array(); // create array what will consist model errors
-
-        if ($truncateMode) { // if truncated mode ON
-            echo "\033[34m Wait database truncating... \033[0m \n\n";
-            require_once $this->php_sql_parser; // require sql parser
-            $keys = array(); // prepare array for store FK`s data
-            foreach (Yii::app()->db->schema->getTables() as $tableSchema) { // Run over all tables
-                $parser = new PHPSQLParser();
-                $result = Yii::app()->db->createCommand('SHOW CREATE TABLE ' . $tableSchema->name)->queryRow(); // getting create sql statement for current table
-                $parsed = $parser->parse($result['Create Table']); //parse sql create statement
-                $table = $parsed['TABLE']['base_expr']; // get table name
-                foreach ($parsed['TABLE']['create-def']['sub_tree'] as $key => $column_def) { // run over all columns of the table
-                    if ($column_def['expr_type'] != 'foreign-key') { // check if column is FOREIGN KEY
-                        continue;
-                    }
-                    $fkName = $column_def['sub_tree'][0]['sub_tree']['base_expr']; // get FOREIGN KEY name
-                    Yii::app()->db->createCommand()->dropForeignKey(str_replace('`', '', $fkName), str_replace('`', '', $table)); // frop FOREIGN KEY
-                    $keys[] = array( // set FOREIGN KEY data to be recreated
-                        'name'       => str_replace('`', '', $column_def['sub_tree'][0]['sub_tree']['base_expr']),
-                        'table'      => str_replace('`', '', $table),
-                        'column'     => $column_def['sub_tree'][3]['sub_tree'][0]['no_quotes'],
-                        'ref_table'  => $column_def['sub_tree'][4]['sub_tree'][1]['no_quotes'],
-                        'ref_column' => $column_def['sub_tree'][4]['sub_tree'][2]['sub_tree'][0]['no_quotes'],
-                        'update'     => $column_def['sub_tree'][4]['sub_tree'][5]['base_expr'],
-                        'delete'     => $column_def['sub_tree'][4]['sub_tree'][5]['base_expr'],
-                    );
-                }
+        // import models classes to make available create new instances
+        if (is_array($this->modelsFolder)) {
+            foreach ($this->modelsFolder as $folder) {
+                Yii::import($folder); //import each folder form array
             }
-            foreach (Yii::app()->db->schema->getTables() as $tableSchema) { // after drop all FK`s truncate each table
-                Yii::app()->db->createCommand()->truncateTable(str_replace('`', '', $tableSchema->name));
-            }
-            foreach ($keys as $identifier => $foreignKey) { // recreate all FK`s to make sure schema is safe
-                Yii::app()->db->createCommand()->addForeignKey($foreignKey['name'],
-                    $foreignKey['table'],
-                    $foreignKey['column'],
-                    $foreignKey['ref_table'],
-                    $foreignKey['ref_column'],
-                    $foreignKey['update'],
-                    $foreignKey['delete']
-                );
-            }
+        } else {
+            Yii::import(empty($this->modelsFolder) ? 'application.models.*' : $this->modelsFolder);
         }
+
+        $fixtures  = require_once $this->pathToFixtures; // require that file with fixtures, will be array
+        $errorList = array();
 
         foreach ($fixtures as $modelClass => $instances) { // run through the array with fixtures
             $modelClass::model()->deleteAll(); // removing old rows from database if database is not truncated
@@ -119,9 +66,9 @@ class EDbFixtureManager extends CConsoleCommand
         if (!empty($errorList)) { // if error list not empty
             echo "\033[31m Validation errors occurs during loading the fixtures, some fixtures wasn't loaded to database \033[0m  \n
                     \033[33m  The next errors occur \033[0m \n";
-            foreach ($errorList as $key => $errors) { // run over all errors and display error what occur during saving into db
-                foreach ($errors as $k => $error) {
-                    foreach ($error as $i => $value) {
+            foreach ($errorList as $errors) { // run over all errors and display error what occur during saving into db
+                foreach ($errors as $error) {
+                    foreach ($error as $value) {
                         echo "\033[37;41m" . $value . "\033[0m   \n"; //display error
                     }
                 }
@@ -129,11 +76,7 @@ class EDbFixtureManager extends CConsoleCommand
             die;
         }
 
-        echo "\033[37;42m All fixtures loaded properly \033[0m   \n
-              Script execution time: " . round(microtime(true) - $startTime, 2) . " ms.  \n";
-
-        $endMemory = round(memory_get_usage(true) / 1048576, 2);
-        echo "Memory used: " . ($endMemory - $startMemory) . "mb \n";
+        echo "\033[37;42m All fixtures loaded properly \033[0m \n";
     }
 
     /**
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Is a tool which provides an ability to use fixtures with Yii framework.
 
 1) All attributes you want to fill via fixtures data, must be defined with `safe` validation rule (`rules()` method);
 2) Don't forget configure `tablePrefix` option for `db` connection definition;
-3) Your tables will be purged when you loading fixtures.
+3) Your tables will be purged when you loading fixtures;
 
 ### Basic usage:
 1) Download extension and place it in `extensions` directory;
@@ -48,26 +48,3 @@ NOTE. If you have a multiple models folder you can specify `modelsFolder` as an 
 E.g. ` 'modelsFolder'   => array('application.models.*', 'application.modules.user.models.*') ,`
 
 4) Run command: `php path/to/yiic fixtures load` ;
-
-### Advanced usage:
-
-If you want to truncate your tables instead of deleting rows from database:
-
-  A) Download PHPSQLParser (you may download it from [here](https://code.google.com/archive/p/php-sql-parser));
-  B) Place the extension folder where ever in you application, but don`t forget to configure settings in console config:
-
-``` php
- 'commandMap' => array(
-     'fixtures' => array(
-     ...
-     'php_sql_parser' => '/path/to/PHPSQLParser.php',
-```
-  C) Run cli command with option: `php protected/yiic fixtures load --truncateMode=true` ;
-
-TO DO list
-===============
-1) Add developer friendly messages about non-existing models or tables;
-
-2) Add relation support;
-
-3) Find a better way to truncate the database.__
